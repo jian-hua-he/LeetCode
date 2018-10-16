1d16ed42da4c09d80524a226af61c8b36e21847b
Jian-Hua, He <game09116@gmail.com>
2018-10-16 21:55:01 +0800

    Add testing

--- addTwoNumbers/main_test.go
+++ addTwoNumbers/main_test.go
@@ -3,5 +3,51 @@ package main
 import "testing"
 
 func TestAddTwoNumbers(t *testing.T) {
-	t.Error("Not implement")
+	l1 := &ListNode{
+		Val: 2,
+		Next: &ListNode{
+			Val: 4,
+			Next: &ListNode{
+				Val:  3,
+				Next: nil,
+			},
+		},
+	}
+	l2 := &ListNode{
+		Val: 5,
+		Next: &ListNode{
+			Val: 6,
+			Next: &ListNode{
+				Val:  4,
+				Next: nil,
+			},
+		},
+	}
+
+	r1 := addTwoNumbers(l1, l2)
+	if r1 == nil {
+		t.Errorf("Data was incorrect, got %v, want reference of ListNode", r1)
+		return
+	}
+	if r1.Val != 7 {
+		t.Errorf("Data was incorrect, got %v, want %v", r1.Val, 7)
+	}
+
+	if r1.Next == nil {
+		t.Errorf("Data was incorrect, got %v, want reference of ListNode", r1.Next)
+		return
+	}
+	r1 = r1.Next
+	if r1.Val != 0 {
+		t.Errorf("Data was incorrect, got %v, want %v", r1.Val, 0)
+	}
+
+	if r1.Next == nil {
+		t.Errorf("Data was incorrect, got %v, want reference of ListNode", r1.Next)
+		return
+	}
+	r1 = r1.Next
+	if r1.Val != 8 {
+		t.Errorf("Data was incorrect, got %v, want %v", r1.Val, 8)
+	}
 }
