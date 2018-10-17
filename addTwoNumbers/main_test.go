b3d8d671adf0904a2086f30d15495278b70a59a5
Jian-Hua, He <game09116@gmail.com>
2018-10-17 22:08:51 +0800

    Add new test

--- addTwoNumbers/main_test.go
+++ addTwoNumbers/main_test.go
@@ -2,7 +2,32 @@ package main
 
 import "testing"
 
-func TestAddTwoNumbers(t *testing.T) {
+func TestArrToListNode(t *testing.T) {
+	nums1 := []int{1, 2, 3}
+
+	l1 := arrToListNode(nums1)
+	t1 := l1
+
+	if t1.Val != 1 {
+		t.Errorf("Data was incorrect, got %v, want %v", t1.Val, 1)
+	}
+
+	t1 = t1.Next
+	if t1.Val != 2 {
+		t.Errorf("Data was incorrect, got %v, want %v", t1.Val, 2)
+	}
+
+	t1 = t1.Next
+	if t1.Val != 3 {
+		t.Errorf("Data was incorrect, got %v, want %v", t1.Val, 3)
+	}
+
+	if t1.Next != nil {
+		t.Errorf("Data was incorrect, got %v, want %v", t1.Next, nil)
+	}
+}
+
+func TestAddTwoNumbersCase1(t *testing.T) {
 	l1 := &ListNode{
 		Val: 2,
 		Next: &ListNode{
