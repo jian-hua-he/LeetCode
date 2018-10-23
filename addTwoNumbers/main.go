aa096a0435e41414a7df5f25c1fc5dffe42cd4d3
Jian-Hua, He <game09116@gmail.com>
2018-10-23 12:08:36 +0800

    Implement addTwoNumbersAlpha

--- addTwoNumbers/main.go
+++ addTwoNumbers/main.go
@@ -80,3 +80,33 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 
 	return result
 }
+
+func addTwoNumbersAlpha(l1 *ListNode, l2 *ListNode) *ListNode {
+	a1 := listNodeToArr(l1)
+	a2 := listNodeToArr(l2)
+
+	a1Len := len(a1)
+	a2Len := len(a2)
+	max := a1Len
+	if a2Len > a1Len {
+		max = a2Len
+	}
+
+	arr := make([]int, max)
+	for i := 0; i < max; i++ {
+		if i < a1Len {
+			arr[i] += a1[i]
+		}
+
+		if i < a2Len {
+			arr[i] += a2[i]
+		}
+
+		if arr[i] >= 10 {
+			arr[i+1]++
+			arr[i] -= 10
+		}
+	}
+
+	return arrToListNode(arr)
+}
