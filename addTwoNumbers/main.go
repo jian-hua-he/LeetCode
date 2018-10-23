c859cfcabb928b6d35015cc3a1e12776b3174538
Jian-Hua, He <game09116@gmail.com>
2018-10-23 13:59:33 +0800

    Replace beta to origin one

--- addTwoNumbers/main.go
+++ addTwoNumbers/main.go
@@ -34,36 +34,6 @@ func listNodeToArr(l *ListNode) []int {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	a1 := listNodeToArr(l1)
-	a2 := listNodeToArr(l2)
-
-	a1Len := len(a1)
-	a2Len := len(a2)
-	max := a1Len
-	if a2Len > a1Len {
-		max = a2Len
-	}
-
-	arr := make([]int, max)
-	for i := 0; i < max; i++ {
-		if i < a1Len {
-			arr[i] += a1[i]
-		}
-
-		if i < a2Len {
-			arr[i] += a2[i]
-		}
-
-		if arr[i] >= 10 {
-			arr[i+1]++
-			arr[i] -= 10
-		}
-	}
-
-	return arrToListNode(arr)
-}
-
-func addTwoNumbersBeta(l1 *ListNode, l2 *ListNode) *ListNode {
 	t1 := l1
 	t2 := l2
 
