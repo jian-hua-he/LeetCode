882254d48e9f8e0205c41e4c6e7a5c686bfb81d5
Jian-Hua, He <game09116@gmail.com>
2018-10-23 13:30:47 +0800

    Add new test

--- addTwoNumbers/main_test.go
+++ addTwoNumbers/main_test.go
@@ -187,3 +187,32 @@ func TestAddTwoNumbersCase2(t *testing.T) {
 		t.Errorf("Data was incorrect, got %v, want %v", r1.Val, 1)
 	}
 }
+
+func TestAddTwoNumbersCase3(t *testing.T) {
+	l1 := &ListNode{
+		Val:  5,
+		Next: nil,
+	}
+	l2 := &ListNode{
+		Val:  5,
+		Next: nil,
+	}
+
+	r1 := addTwoNumbers(l1, l2)
+	if r1 == nil {
+		t.Errorf("Data was incorrect, got %v, want reference of ListNode", r1)
+		return
+	}
+	if r1.Val != 1 {
+		t.Errorf("Data was incorrect, got %v, want %v", r1.Val, 1)
+	}
+
+	r1 = r1.Next
+	if r1 == nil {
+		t.Errorf("Data was incorrect, got %v, want reference of ListNode", r1)
+		return
+	}
+	if r1.Val != 1 {
+		t.Errorf("Data was incorrect, got %v, want %v", r1.Val, 1)
+	}
+}
