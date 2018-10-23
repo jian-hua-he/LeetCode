b86a405fe7678c4983d0d110925d69f2477b991b
Jian-Hua, He <game09116@gmail.com>
2018-10-23 13:37:38 +0800

    Add addTwoNumberBeta

--- addTwoNumbers/main.go
+++ addTwoNumbers/main.go
@@ -62,3 +62,41 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 
 	return arrToListNode(arr)
 }
+
+func addTwoNumbersBeta(l1 *ListNode, l2 *ListNode) *ListNode {
+	t1 := l1
+	t2 := l2
+
+	result := &ListNode{
+		Val:  0,
+		Next: nil,
+	}
+	tmp := result
+
+	for t1 != nil && t2 != nil {
+
+		if t1 != nil {
+			tmp.Val += t1.Val
+			t1 = t1.Next
+		}
+
+		if t2 != nil {
+			tmp.Val += t2.Val
+			t2 = t2.Next
+		}
+
+		nextVal := 0
+		if tmp.Val >= 10 {
+			nextVal++
+			tmp.Val -= 10
+		}
+
+		tmp.Next = &ListNode{
+			Val:  nextVal,
+			Next: nil,
+		}
+		tmp = tmp.Next
+	}
+
+	return result
+}
