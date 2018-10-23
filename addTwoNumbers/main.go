5a557c7eb18973b20acbbbb2b7625a32f278c7e7
Jian-Hua, He <game09116@gmail.com>
2018-10-23 13:30:39 +0800

    Replace addTwoNumbers with addTwoNumbersAlpha

--- addTwoNumbers/main.go
+++ addTwoNumbers/main.go
@@ -1,48 +1,8 @@
 package main
 
-import (
-	"strconv"
-)
-
 type ListNode struct {
 	Val  int
 	Next *ListNode
-}
-
-func toNum(l *ListNode) (result int, err error) {
-	str := ""
-
-	list := l
-	for list != nil {
-		str = strconv.Itoa(list.Val) + str
-		list = list.Next
-	}
-
-	result, err = strconv.Atoi(str)
-
-	return
-}
-
-func numToListNode(num int) (result *ListNode, err error) {
-	str := strconv.Itoa(num)
-	result = new(ListNode)
-
-	curr := result
-	for i := len(str) - 1; i >= 0; i-- {
-		n, err := strconv.Atoi(string(str[i]))
-
-		if err != nil {
-			return nil, err
-		}
-
-		curr.Val = n
-		if i != 0 {
-			curr.Next = new(ListNode)
-			curr = curr.Next
-		}
-	}
-
-	return result, nil
 }
 
 func arrToListNode(nums []int) *ListNode {
@@ -74,14 +34,6 @@ func listNodeToArr(l *ListNode) []int {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	n1, _ := toNum(l1)
-	n2, _ := toNum(l2)
-	result, _ := numToListNode(n1 + n2)
-
-	return result
-}
-
-func addTwoNumbersAlpha(l1 *ListNode, l2 *ListNode) *ListNode {
 	a1 := listNodeToArr(l1)
 	a2 := listNodeToArr(l2)
 
