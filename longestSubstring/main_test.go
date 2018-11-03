571a6179f56cd5f17a7e15c380d8eeb12e5edfe7
Jian-Hua, He <game09116@gmail.com>
2018-11-03 14:54:27 +0800

    Add new test

--- longestSubstring/main_test.go
+++ longestSubstring/main_test.go
@@ -9,20 +9,27 @@ func TestLengthOfLongestSubstring(t *testing.T) {
 	expected1 := 3
 	len1 := lengthOfLongestSubstring(input1)
 	if len1 != expected1 {
-		t.Errorf("Data was incorrect, input %v, got %v, want %v", input1, len1, expected1)
+		t.Errorf("Data was incorrect, input \"%v\", got %v, want %v", input1, len1, expected1)
 	}
 
 	input2 := "bbbbb"
 	expected2 := 1
 	len2 := lengthOfLongestSubstring(input2)
 	if len2 != expected2 {
-		t.Errorf("Data was incorrect, input %v, got %v, want %v", input2, len2, expected2)
+		t.Errorf("Data was incorrect, input \"%v\", got %v, want %v", input2, len2, expected2)
 	}
 
 	input3 := "pwwkew"
 	expected3 := 3
 	len3 := lengthOfLongestSubstring(input3)
 	if len3 != expected3 {
-		t.Errorf("Data was incorrect, input %v, got %v, want %v", input3, len3, expected3)
+		t.Errorf("Data was incorrect, input \"%v\", got %v, want %v", input3, len3, expected3)
+	}
+
+	input4 := " "
+	expected4 := 1
+	len4 := lengthOfLongestSubstring(input4)
+	if len4 != expected4 {
+		t.Errorf("Data was incorrect, input \"%v\", got %v, want %v", input4, len4, expected4)
 	}
 }
