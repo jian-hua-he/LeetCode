6f3a05c5dab20be6ed82bed9f2b7e4b9c033fb07
Jian-Hua, He <game09116@gmail.com>
2018-11-03 14:34:45 +0800

    Implement testing

--- longestSubstring/main.go
+++ longestSubstring/main.go
@@ -1,1 +1,5 @@
 package main
+
+func lengthOfLongestSubstring(s string) int {
+	return 0
+}
--- longestSubstring/main_test.go
+++ longestSubstring/main_test.go
@@ -4,6 +4,19 @@ import (
 	"testing"
 )
 
-func TestLongestSubstring(t *testing.T) {
-	t.Error("Not implement")
+func TestLengthOfLongestSubstring(t *testing.T) {
+	len1 := lengthOfLongestSubstring("abcabcbb")
+	if len1 != 3 {
+		t.Errorf("Data was incorrect, got %v, want %v", len1, 3)
+	}
+
+	len2 := lengthOfLongestSubstring("bbbbb")
+	if len2 != 1 {
+		t.Errorf("Data was incorrect, got %v, want %v", len2, 1)
+	}
+
+	len3 := lengthOfLongestSubstring("pwwkew")
+	if len3 != 3 {
+		t.Errorf("Data was incorrect, got %v, want %v", len3, 3)
+	}
 }
