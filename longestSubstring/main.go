29475e35575a9d5d6ca20b701b9b826a99c5b399
Jian-Hua, He <game09116@gmail.com>
2018-11-03 15:13:33 +0800

    Update implementation

--- longestSubstring/main.go
+++ longestSubstring/main.go
@@ -3,27 +3,22 @@ package main
 func lengthOfLongestSubstring(s string) int {
 	longest := 0
 
-	if len(s) == 1 {
-		return 1
-	}
+	for i := 0; i < len(s); i++ {
+		temp := map[string]string{}
+		for j := i; j < len(s); j++ {
+			str := string(s[j])
+			_, exists := temp[str]
 
-	temp := map[string]string{}
-	for _, v := range s {
-		str := string(v)
-
-		if _, ok := temp[str]; ok {
-			if len(temp) > longest {
-				longest = len(temp)
+			if exists {
+				break
 			}
 
-			temp = map[string]string{}
+			temp[str] = str
 		}
 
-		temp[str] = str
-	}
-
-	if len(temp) > longest {
-		longest = len(temp)
+		if len(temp) > longest {
+			longest = len(temp)
+		}
 	}
 
 	return longest
