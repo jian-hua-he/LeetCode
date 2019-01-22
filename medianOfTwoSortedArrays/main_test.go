c21b9ca58338612db9539d0585fe7a40b1a9bb37
Jian-Hua, He <game09116@gmail.com>
2019-01-22 13:36:27 +0800

    Add empty test

--- medianOfTwoSortedArrays/main_test.go
+++ medianOfTwoSortedArrays/main_test.go
@@ -1,1 +1,23 @@
 package main
+
+import "testing"
+
+func TestEmptyTwoArray(t *testing.T) {
+	t.Errorf("Not Implement")
+}
+
+func TestEmptyOneArray(t *testing.T) {
+	t.Errorf("Not Implement")
+}
+
+func TestCaseOne(t *testing.T) {
+	t.Errorf("Not Implement")
+}
+
+func TestCaseTwo(t *testing.T) {
+	t.Errorf("Not Implement")
+}
+
+func TestCaseThree(t *testing.T) {
+	t.Errorf("Not Implement")
+}
