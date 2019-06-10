d45b815f66e56fc4aa2f88ef2b250d1f0e8955d3
Jian-Hua, He <game09116@gmail.com>
2019-06-10 15:50:27 +0800

    First version

--- longestPalindrome/main.go
+++ longestPalindrome/main.go
@@ -1,5 +1,47 @@
 package main
 
 func longestPalindrome(s string) string {
+	l := len(s)
+
+	for {
+		if l == 0 {
+			break
+		}
+
+		i, j := 0, l-1
+		for {
+			if j >= len(s) {
+				break
+			}
+
+			if isPalindrome(s[i:j]) {
+				return s[i:j]
+			}
+
+			i += 1
+			j += 1
+		}
+
+		l -= 1
+	}
+
 	return ""
+}
+
+func isPalindrome(s string) bool {
+	if len(s) <= 1 {
+		return false
+	}
+
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		if i == j {
+			break
+		}
+
+		if s[i] != s[j] {
+			return false
+		}
+	}
+
+	return true
 }
