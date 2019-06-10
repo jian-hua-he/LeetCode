a849ebaa1619a2413f64635e94b3383d27f37fb3
Jian-Hua, He <game09116@gmail.com>
2019-06-10 21:26:30 +0800

    Fix loop condition

--- longestPalindrome/main.go
+++ longestPalindrome/main.go
@@ -2,7 +2,7 @@ package main
 
 func longestPalindrome(s string) string {
 	for l := len(s); l > 0; l -= 1 {
-		for i, j := 0, l-1; j < len(s); i, j = i+1, j+1 {
+		for i, j := 0, l; j <= len(s); i, j = i+1, j+1 {
 			if isPalindrome(s[i:j]) {
 				return s[i:j]
 			}
@@ -13,11 +13,7 @@ func longestPalindrome(s string) string {
 }
 
 func isPalindrome(s string) bool {
-	if len(s) <= 1 {
-		return false
-	}
-
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+	for i, j := 0, len(s)-1; i <= j; i, j = i+1, j-1 {
 		if i == j {
 			break
 		}
