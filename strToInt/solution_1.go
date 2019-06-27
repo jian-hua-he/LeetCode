6688ef210814e00bfe933d1276d4af3ed2e7a194
Jian-Hua, He <game09116@gmail.com>
2019-06-27 18:14:07 +0800

    First version

--- strToInt/solution_1.go
+++ strToInt/solution_1.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 func myAtoi1(str string) int {
-	return 0
+	var result int32 = 0
+
+	re := regexp.MustCompile(`^\s*\-?\d*`)
+	s := re.FindString(str)
+	s = strings.Trim(s, " ")
+	r, _ := strconv.Atoi(s)
+	result = int32(r)
+
+	return int(result)
 }
