89732b9442a620adb840297dda280933dca90152
Jian-Hua, He <game09116@gmail.com>
2019-06-27 18:16:13 +0800

    Fix int max and min issue

--- strToInt/solution_1.go
+++ strToInt/solution_1.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"math"
 	"regexp"
 	"strconv"
 	"strings"
 )
 
 func myAtoi1(str string) int {
-	var result int32 = 0
-
 	re := regexp.MustCompile(`^\s*\-?\d*`)
 	s := re.FindString(str)
 	s = strings.Trim(s, " ")
 	r, _ := strconv.Atoi(s)
-	result = int32(r)
 
-	return int(result)
+	if r > math.MaxInt32 {
+		return math.MaxInt32
+	}
+
+	if r < math.MinInt32 {
+		return math.MinInt32
+	}
+
+	return r
 }
