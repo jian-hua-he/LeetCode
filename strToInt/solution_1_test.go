e50b2a2620c187ad07e43c9d10a8cc813dd6408e
Jian-Hua, He <game09116@gmail.com>
2019-06-27 17:47:56 +0800

    Add more test

--- strToInt/solution_1_test.go
+++ strToInt/solution_1_test.go
@@ -2,9 +2,49 @@ package main
 
 import "testing"
 
-func TestCaseOne(t *testing.T) {
+func TestCase1ForSolution1(t *testing.T) {
 	input := "42"
 	want := 42
+	got := myAtoi1(input)
+
+	if got != want {
+		t.Errorf("Data was incorrect, input \"%v\", got \"%v\", want \"%v\"", input, got, want)
+	}
+}
+
+func TestCase2ForSolution1(t *testing.T) {
+	input := "-42"
+	want := -42
+	got := myAtoi1(input)
+
+	if got != want {
+		t.Errorf("Data was incorrect, input \"%v\", got \"%v\", want \"%v\"", input, got, want)
+	}
+}
+
+func TestCase3ForSolution1(t *testing.T) {
+	input := "   -42"
+	want := -42
+	got := myAtoi1(input)
+
+	if got != want {
+		t.Errorf("Data was incorrect, input \"%v\", got \"%v\", want \"%v\"", input, got, want)
+	}
+}
+
+func TestCase4ForSolution1(t *testing.T) {
+	input := "words and 987"
+	want := 0
+	got := myAtoi1(input)
+
+	if got != want {
+		t.Errorf("Data was incorrect, input \"%v\", got \"%v\", want \"%v\"", input, got, want)
+	}
+}
+
+func TestCase5ForSolution1(t *testing.T) {
+	input := "-91283472332"
+	want := -2147483648
 	got := myAtoi1(input)
 
 	if got != want {
