6b128157768e205c434bcecad025ff2268c6f2a3
Jian-Hua, He <game09116@gmail.com>
2019-06-30 14:46:19 +0800

    Update solution 2

--- longestPalindrome/solution_2.go
+++ longestPalindrome/solution_2.go
@@ -8,22 +8,30 @@ func longestPalindrome2(s string) string {
 	start, end := 0, 0
 
 	for i := 0; i < len(s); i += 1 {
-		len1 := expandAroundCenter(s, i, i)
-		len2 := expandAroundCenter(s, i, i+1)
+		l1, r1 := expandAroundCenter(s, i, i)
+		l2, r2 := expandAroundCenter(s, i, i+1)
+		len1 := r1 - l1 + 1
+		len2 := r2 - l2 + 1
+
 		longest := len1
+		l := l1
+		r := r1
 		if len2 > len1 {
 			longest = len2
+			l = l2
+			r = r2
 		}
+
 		if longest > end-start {
-			start = i - (longest-1)/2
-			end = i + longest/2
+			start = l
+			end = r
 		}
 	}
 
 	return s[start : end+1]
 }
 
-func expandAroundCenter(s string, left int, right int) int {
+func expandAroundCenter(s string, left int, right int) (int, int) {
 	l, r := left, right
 
 	for l >= 0 && r < len(s) && s[l] == s[r] {
@@ -31,5 +39,5 @@ func expandAroundCenter(s string, left int, right int) int {
 		r += 1
 	}
 
-	return r - l - 1
+	return l + 1, r - 1
 }
