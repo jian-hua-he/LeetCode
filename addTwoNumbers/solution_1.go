41aac2340f98ccb93a6bd0e2aef97ae12705efb1
Jian-Hua, He <game09116@gmail.com>
2019-06-30 12:34:59 +0800

    Code refactoring

--- addTwoNumbers/solution_1.go
+++ addTwoNumbers/solution_1.go
@@ -5,6 +5,55 @@ type ListNode struct {
 	Next *ListNode
 }
 
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	t1 := l1
+	t2 := l2
+
+	result := &ListNode{
+		Val:  0,
+		Next: nil,
+	}
+
+	for curr := result; t1 != nil || t2 != nil; curr = curr.Next {
+
+		// Calculate value and go to next for input linked list
+		if t1 != nil {
+			curr.Val += t1.Val
+			t1 = t1.Next
+		}
+
+		if t2 != nil {
+			curr.Val += t2.Val
+			t2 = t2.Next
+		}
+
+		// Handle next
+		nextVal := 0
+		if curr.Val >= 10 {
+			nextVal += 1
+			curr.Val -= 10
+			curr.Next = &ListNode{
+				Val:  nextVal,
+				Next: nil,
+			}
+			continue
+		}
+
+		if t1 == nil && t2 == nil {
+			curr.Next = nil
+			continue
+		}
+
+		curr.Next = &ListNode{
+			Val:  0,
+			Next: nil,
+		}
+	}
+
+	return result
+}
+
+// Helpers
 func arrToListNode(nums []int) *ListNode {
 	result := new(ListNode)
 
@@ -28,56 +77,6 @@ func listNodeToArr(l *ListNode) []int {
 	for temp != nil {
 		result = append(result, temp.Val)
 		temp = temp.Next
-	}
-
-	return result
-}
-
-func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	t1 := l1
-	t2 := l2
-
-	result := &ListNode{
-		Val:  0,
-		Next: nil,
-	}
-	tmp := result
-
-	for t1 != nil || t2 != nil {
-
-		if t1 != nil {
-			tmp.Val += t1.Val
-			t1 = t1.Next
-		}
-
-		if t2 != nil {
-			tmp.Val += t2.Val
-			t2 = t2.Next
-		}
-
-		nextVal := 0
-		if tmp.Val >= 10 {
-			nextVal++
-			tmp.Val -= 10
-		}
-
-		if nextVal > 0 {
-			tmp.Next = &ListNode{
-				Val:  nextVal,
-				Next: nil,
-			}
-		} else {
-			if t1 == nil && t2 == nil {
-				tmp.Next = nil
-			} else {
-				tmp.Next = &ListNode{
-					Val:  0,
-					Next: nil,
-				}
-			}
-		}
-
-		tmp = tmp.Next
 	}
 
 	return result
