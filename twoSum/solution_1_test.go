79466afc33a4fe3a965bb3c0173d0185fa4f3ee9
Jian-Hua, He <game09116@gmail.com>
2019-07-04 10:45:43 +0800

    Update test

--- twoSum/solution_1_test.go
+++ twoSum/solution_1_test.go
@@ -2,34 +2,72 @@ package main
 
 import "testing"
 
-func TestTwoNum(t *testing.T) {
-	t1 := []int{2, 7, 11, 15}
-	r1 := twoSum(t1, 9)
+func TestCase1TwoSum(t *testing.T) {
+	input := []int{2, 7, 11, 15}
+	got := twoSum(input, 9)
 
-	if r1[0] != 0 {
-		t.Errorf("Data was incorrect, got %v, want %v", r1[0], 0)
+	if (got[0] != 0 || got[1] != 1) && (got[0] != 1 || got[1] != 0) {
+		t.Errorf(
+			"Data was incorrect, got %v, want %v or %v",
+			got,
+			[]int{0, 1},
+			[]int{1, 0},
+		)
 	}
-	if r1[1] != 1 {
-		t.Errorf("Data was incorrect, got %v, want %v", r1[1], 1)
-	}
+}
 
-	t2 := []int{2, 5, 10, 12}
-	r2 := twoSum(t2, 14)
+func TestCase2TwoSum(t *testing.T) {
+	input := []int{2, 5, 10, 12}
+	got := twoSum(input, 14)
 
-	if r2[0] != 0 {
-		t.Errorf("Data was incorrect, got %v, want %v", r2[0], 0)
+	if (got[0] != 0 || got[1] != 3) && (got[0] != 3 || got[1] != 0) {
+		t.Errorf(
+			"Data was incorrect, got %v, want %v or %v",
+			got,
+			[]int{0, 3},
+			[]int{3, 0},
+		)
 	}
-	if r2[1] != 3 {
-		t.Errorf("Data was incorrect, got %v, want %v", r2[1], 3)
-	}
+}
 
-	t3 := []int{3, 2, 4}
-	r3 := twoSum(t3, 6)
+func TestCase3TwoSum(t *testing.T) {
+	input := []int{3, 2, 4}
+	got := twoSum(input, 6)
 
-	if r3[0] != 1 {
-		t.Errorf("Data was incorrect, got %v, want %v", r3[0], 1)
+	if (got[0] != 1 || got[1] != 2) && (got[0] != 2 || got[1] != 1) {
+		t.Errorf(
+			"Data was incorrect, got %v, want %v or %v",
+			got,
+			[]int{1, 2},
+			[]int{2, 1},
+		)
 	}
-	if r3[1] != 2 {
-		t.Errorf("Data was incorrect, got %v, want %v", r3[1], 2)
+}
+
+func TestCase4TwoSum(t *testing.T) {
+	input := []int{4, 6, 12, 5, 2}
+	got := twoSum(input, 11)
+
+	if (got[0] != 1 || got[1] != 3) && (got[0] != 3 || got[1] != 1) {
+		t.Errorf(
+			"Data was incorrect, got %v, want %v or %v",
+			got,
+			[]int{1, 3},
+			[]int{3, 1},
+		)
+	}
+}
+
+func TestCase5TwoSum(t *testing.T) {
+	input := []int{-1, -2, -3, -4, -5}
+	got := twoSum(input, -8)
+
+	if (got[0] != 2 || got[1] != 4) && (got[0] != 4 || got[1] != 2) {
+		t.Errorf(
+			"Data was incorrect, got %v, want %v or %v",
+			got,
+			[]int{2, 4},
+			[]int{4, 2},
+		)
 	}
 }
