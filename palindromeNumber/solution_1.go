2f57e2a9e7b3f28eda1782ab2b01fe1296c6f968
Jian-Hua, He <game09116@gmail.com>
2019-07-09 21:21:10 +0800

    Implement solution

--- palindromeNumber/solution_1.go
+++ palindromeNumber/solution_1.go
@@ -1,5 +1,22 @@
 package main
 
 func isPalindrome(x int) bool {
-	return false
+	if x < 0 {
+		return false
+	}
+
+	arr := make([]int, 0)
+
+	for n := x; n > 0; n /= 10 {
+		remainer := n % 10
+		arr = append(arr, remainer)
+	}
+
+	for i, j := 0, len(arr)-1; i <= j; i, j = i+1, j-1 {
+		if arr[i] != arr[j] {
+			return false
+		}
+	}
+
+	return true
 }
