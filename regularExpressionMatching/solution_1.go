21cd4a0378e28f497b3e2f18bd7e1fd23b4d4f8c
Jian-Hua, He <game09116@gmail.com>
2019-07-16 17:02:55 +0800

    Update solution

--- regularExpressionMatching/solution_1.go
+++ regularExpressionMatching/solution_1.go
@@ -1,64 +1,15 @@
 package main
 
-import "fmt"
-
 func isMatch(s string, p string) bool {
-	i := 0
-	curr := 0
-
-	for curr < len(p) {
-		// In '*' situation
-		if curr+1 < len(p) && string(p[curr+1]) == "*" {
-			if i >= len(s) || (s[i] != p[curr] && string(p[curr]) != ".") {
-				curr += 2
-			} else {
-				i += 1
-			}
-
-			continue
-		}
-
-		// Check the index if it out of range
-		if i == len(s) {
-			fmt.Println("=====")
-			fmt.Println("i is out of range")
-			fmt.Println("=====")
-			return false
-		}
-
-		// Validate string
-		if !inLowercaseAToZ(s[i]) {
-			fmt.Println("=====")
-			fmt.Println("s[i] is invalid")
-			fmt.Println("=====")
-			return false
-		}
-
-		// In `.` situation
-		if string(p[curr]) == "." {
-			i += 1
-			curr += 1
-			continue
-		}
-
-		if p[curr] == s[i] {
-			i += 1
-			curr += 1
-		} else {
-			fmt.Println("=====")
-			fmt.Println("p[curr] != s[i]")
-			fmt.Println("=====")
-			return false
-		}
+	if p == "" {
+		return s == ""
 	}
 
-	if i != len(s) {
-		return false
+	firstMatch := s != "" && (s[0] == p[0] || string(p[0]) == ".")
+
+	if len(p) >= 2 && string(p[1]) == "*" {
+		return isMatch(s, p[2:]) || firstMatch && isMatch(s[1:], p)
 	}
 
-	return true
-}
-
-func inLowercaseAToZ(s byte) bool {
-	return s >= 97 && s <= 122
+	return firstMatch && isMatch(s[1:], p[1:])
 }
